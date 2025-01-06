15d8530228e2e244f65259b3bde6138b2e0771b1
Hung Mai <hung.mai@asnet.com.vn>
2025-01-06 10:14:44 +0700

    Implement metadata crawl datadata

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://shopping-ecom-gi32.vercel.app/sitemap.xml',
+  };
+}
---
+++ src/app/sidemap.ts
@@ -0,0 +1,42 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://shopping-ecom-gi32.vercel.app/',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://shopping-ecom-gi32.vercel.app/contact',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://shopping-ecom-gi32.vercel.app/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://shopping-ecom-gi32.vercel.app/wishlist',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://shopping-ecom-gi32.vercel.app/cart',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://shopping-ecom-gi32.vercel.app/account',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://shopping-ecom-gi32.vercel.app/checkout',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://shopping-ecom-gi32.vercel.app/sign-in',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://shopping-ecom-gi32.vercel.app/sign-up',
+      lastModified: new Date(),
+    },
+  ]
+}

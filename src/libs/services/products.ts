d8d7179d78d4b1538c42933ef3436245d34cc4c0
Hung Mai <hung.mai@asnet.com.vn>
2025-01-12 22:35:35 +0700

    Update get product Id

--- src/libs/services/products.ts
+++ src/libs/services/products.ts
@@ -1,16 +1,29 @@
-import { END_POINT } from "@/constants";
-import { fetchData, fetchDataId } from "@/libs/services/api";
+import { END_POINT } from '@/constants';
+import { fetchData, fetchDataId } from '@/libs/services/api';
 
 export const getProducts = async (query?: string) => {
   const param = query ? query : '';
   return await fetchData({ endpoint: `${END_POINT.PRODUCTS}${param}` });
 };
 
-export const getProductLimit = async (query: string, start: number, limit: number) => {
-  return await fetchData({ endpoint: `${END_POINT.PRODUCTS}?${query}&_start=${start}&_limit=${limit}` });
+export const getProductLimit = async (
+  query: string,
+  start: number,
+  limit: number
+) => {
+  return await fetchData({
+    endpoint: `${END_POINT.PRODUCTS}?${query}&_start=${start}&_limit=${limit}`
+  });
 };
 
 export const getProductId = async (id: string) => {
-  const productData = await fetchDataId({ endpoint: `${END_POINT.PRODUCTS}/`, id: id });
-  return { data: productData };
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}${END_POINT.PRODUCTS}/${id}`
+    );
+    const data = await res.json();
+    return { data, error: null };
+  } catch (error) {
+    return { data: null, error };
+  }
 };
